b379b5b8e20bea02fe9e14221f6d25aab866ab0e
Terence Nathan <terenceloorthanathan43@gmail.com>
2023-06-06 15:18:57 +0800

    IDK what changes

---
+++ AlphaZero/AlphaZero/Controllers/InventoriesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AlphaZero.Models;
+
+namespace AlphaZero.Controllers
+{
+    public class InventoriesController : Controller
+    {
+        private db_roomrentalEntities db = new db_roomrentalEntities();
+
+        // GET: Inventories
+        public ActionResult Index()
+        {
+            var inventories = db.inventories.Include(i => i.floor);
+            return View(inventories.ToList());
+        }
+
+        // GET: Inventories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            inventory inventory = db.inventories.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // GET: Inventories/Create
+        public ActionResult Create()
+        {
+            ViewBag.floor_id = new SelectList(db.floors, "floor_id", "floor_cctvQr");
+            return View();
+        }
+
+        // POST: Inventories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Inventory_id,floor_id,inventory_name,inventory_count")] inventory inventory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.inventories.Add(inventory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.floor_id = new SelectList(db.floors, "floor_id", "floor_cctvQr", inventory.floor_id);
+            return View(inventory);
+        }
+
+        // GET: Inventories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            inventory inventory = db.inventories.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.floor_id = new SelectList(db.floors, "floor_id", "floor_cctvQr", inventory.floor_id);
+            return View(inventory);
+        }
+
+        // POST: Inventories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Inventory_id,floor_id,inventory_name,inventory_count")] inventory inventory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(inventory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.floor_id = new SelectList(db.floors, "floor_id", "floor_cctvQr", inventory.floor_id);
+            return View(inventory);
+        }
+
+        // GET: Inventories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            inventory inventory = db.inventories.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // POST: Inventories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            inventory inventory = db.inventories.Find(id);
+            db.inventories.Remove(inventory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
