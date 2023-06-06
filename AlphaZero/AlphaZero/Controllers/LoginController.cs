3a9898c987950c2ad2f05aa9e85dc707e70f16a2
Chongkz29 <120616074+Chongkz29@users.noreply.github.com>
2023-06-06 15:11:09 +0800

    update

---
+++ AlphaZero/AlphaZero/Controllers/LoginController.cs
@@ -0,0 +1,62 @@
+﻿using AlphaZero.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlphaZero.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        db_roomrentalEntities db = new db_roomrentalEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+		public ActionResult Index(user objchk)
+		{
+			if (ModelState.IsValid)
+			{
+				using (db_roomrentalEntities db = new db_roomrentalEntities()) ;
+				{
+					var obj = db.users.Where(a => a.user_name.Equals(objchk.user_name) && a.user_password.Equals(objchk.user_password)).FirstOrDefault();
+
+					if (obj != null)
+					{
+						Session["UserID"] = obj.user_id.ToString();
+						Session["UserName"] = obj.user_name.ToString();
+
+						return RedirectToAction("Index", "Home");
+					}
+
+					else
+					{
+						ModelState.AddModelError("", "The Username or Password Incorrect");
+
+
+					}
+				}
+			}
+
+
+
+
+			return View();
+		}
+		public ActionResult Logout()
+		{
+			Session.Clear();
+			return RedirectToAction("Index", "Login");
+		}
+
+	}
+
+
+}
---
+++ AlphaZero/AlphaZero/Controllers/usersController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AlphaZero.Models;
+
+namespace AlphaZero.Controllers
+{
+    public class usersController : Controller
+    {
+        private db_roomrentalEntities db = new db_roomrentalEntities();
+
+        // GET: users
+        public ActionResult Index()
+        {
+            return View(db.users.ToList());
+        }
+
+        // GET: users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user user = db.users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "user_id,user_password,user_name,user_email,user_type")] user user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        // GET: users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user user = db.users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "user_id,user_password,user_name,user_email,user_type")] user user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            user user = db.users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            user user = db.users.Find(id);
+            db.users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+		public class UserController : Controller
+		{
+			public ActionResult UserList()
+			{
+				// Logic to retrieve the list of users
+
+				return View();
+			}
+
+			[Authorize(Roles = "admin")]
+			public ActionResult DeleteUser(int userId)
+			{
+				// Logic for deleting the user
+
+				return RedirectToAction("UserList");
+			}
+
+			[Authorize(Roles = "admin")]
+			public ActionResult UpdateUser(int userId)
+			{
+				// Logic for updating the user
+
+				return RedirectToAction("UserList");
+			}
+
+			[Authorize(Roles = "admin")]
+			public ActionResult CreateUser()
+			{
+				// Logic for creating a new user
+
+				return RedirectToAction("UserList");
+			}
+		}
+	}
+}
