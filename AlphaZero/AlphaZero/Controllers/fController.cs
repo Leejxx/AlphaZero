9a7f273c74494b3c52c24a38973fbc082a7af923
LowJunyi2001 <120614501+LowJunyi2001@users.noreply.github.com>
2023-06-06 23:44:20 +0800

    update

---
+++ AlphaZero/AlphaZero/Controllers/fController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlphaZero.Controllers
+{
+    public class fController : Controller
+    {
+        // GET: f
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
